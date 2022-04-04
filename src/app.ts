2f154580df2e17f813b3efe1ccc2f0ede3a89cea
tereza-tech <tereza.tech@gmail.com>
2022-04-04 12:30:50 +0000

    Express init

---
+++ src/app.ts
@@ -0,0 +1,32 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.post('/post', (req, res) => {
+  res.send('Got a POST request')
+})
+
+
+app.put('/', (req, res) => {
+  res.send('Got a PUT request at /')
+})
+
+
+app.delete('/', (req, res) => {
+  res.send('Got a DELETE request at /')
+})
+
+
+const server = app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See Swagger: `),
+)
